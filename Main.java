6aae4f14c2a2977f7b7a268bcd780cca8d09080f
Jennet Seyitmyradova <seyitmyradovajennet@gmail.com>
2021-10-25 16:00:32 +0300

    second commit

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args){
         Doctor doctor=new Doctor("Sam ", 34, 4, "Hospital");
         Chef chef=new Chef("Bob ", 25,5, "Restaurant");
-        Teacher teacher=new Teacher("Tom ", 50, 6, 20);
+        Teacher teacher=new Teacher("Tom ", 45, 6, 20);
 
         doctor.getInfo();
         chef.getInfo();
